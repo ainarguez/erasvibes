cba298576ca5884e8c7c147c9cad1b5447995538
ainara <ainara.ro.cu@gmail.com>
2025-05-11 21:54:18 +0200

    Arreglar css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="icon" href="{{ asset(path: 'img/logo.png') }}" type="image/png">
+        <link rel="icon" href="{{ asset('img/logo.png') }}" type="image/png">
 
 
         <!-- Fonts -->
@@ -16,9 +16,9 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('build/assets/estilos.css') }}">
-        <script src="{{ asset('build/assets/app.js') }}" defer></script>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+
 
 
     </head>
