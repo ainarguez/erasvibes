670394cbf46b3557bd89d780df59b1c9c9a123a6
ainara <ainara.ro.cu@gmail.com>
2025-05-11 18:55:41 +0200

    Arreglar css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,32 +4,23 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Vite;
-use Illuminate\Routing\Router;
 use Spatie\Permission\Middleware\RoleMiddleware;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+    /* Register any application services. */
     public function register(): void
     {
-        // Puedes registrar otros bindings aquí si los necesitas
+        $this->app->bind('role', function ($app) {
+            return new RoleMiddleware();
+        });
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(Router $router): void
+    /* Bootstrap any application services. */
+    public function boot(): void
     {
-        // Alias para el middleware 'role'
-        $router->aliasMiddleware('role', RoleMiddleware::class);
-
-        // Forzar HTTPS y usar build en producción
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
-            Vite::useBuildDirectory('build');
         }
     }
 }
